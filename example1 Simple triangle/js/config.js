07fd8a204f30a1d6bbc23ed4d756f66073fe4ad5
Maksim Romaniv <maxim.romaniv@netent.com>
2015-10-23 16:32:13 +0300

    [example 01] added simple example for init webGl

---
+++ example1 Simple triangle/js/config.js
@@ -0,0 +1,20 @@
+var Config = {
+
+    CANVAS_ID : 'canvas',
+
+    Shader : {
+        ROOT_PATH : 'shaders/',
+        FILE_TYPE : '.glsl',
+        elements : [
+            {
+                vertex : {
+                    path : 'vertexShader'
+                },
+                fragment : {
+                    path : 'fragmentShader'
+                }
+            }
+        ]
+    }
+
+};
